fad2bd0bb318a5e4fd6dc78124c158b89389f793
Alejandro Rodrigo FP <alexx.rodrigo.20@gmail.com>
2019-11-04 17:43:03 -0500

    llenado de datos

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -8,18 +8,18 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'gallery', pathMatch: 'prefix' },
-            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
-            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' },
-            { path: 'restaurants', loadChildren: './restaurants/restaurants.module#RestaurantsModule' },
-            { path: 'hotels', loadChildren: './hotels/hotels.module#HotelsModule' },
-            { path: 'festivities', loadChildren: './festivities/festivities.module#FestivitiesModule' },
-            { path: 'gallery', loadChildren: './gallery/gallery.module#GalleryModule' }
+            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+            { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule) },
+            { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
+            { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
+            { path: 'bs-element', loadChildren: () => import('./bs-element/bs-element.module').then(m => m.BsElementModule) },
+            { path: 'grid', loadChildren: () => import('./grid/grid.module').then(m => m.GridModule) },
+            { path: 'components', loadChildren: () => import('./bs-component/bs-component.module').then(m => m.BsComponentModule) },
+            { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule) },
+            { path: 'restaurants', loadChildren: () => import('./restaurants/restaurants.module').then(m => m.RestaurantsModule) },
+            { path: 'hotels', loadChildren: () => import('./hotels/hotels.module').then(m => m.HotelsModule) },
+            { path: 'festivities', loadChildren: () => import('./festivities/festivities.module').then(m => m.FestivitiesModule) },
+            { path: 'gallery', loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule) }
         ]
     }
 ];
